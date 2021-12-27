8a3cad446415cee5259d1121fda1a4922e1eaa5a
Luis <luiscmsousa@gmail.com>
2021-12-27 23:33:26 +0000

    update user interface

---
+++ SUM/Classes/TabBar_ControlViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBar_ControlViewController.swift
+//  SUM
+//
+//  Created by Luís Sousa on 27/12/2021.
+//
+
+import UIKit
+
+class TabBar_ControlViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SUM/Classes/class MyTabBar: UITabBar {  TabBar.swift
@@ -0,0 +1,20 @@
+//
+//  class MyTabBar: UITabBar {  TabBar.swift
+//  SUM
+//
+//  Created by Luís Sousa on 27/12/2021.
+//
+
+import UIKit
+
+class class_MyTabBar__UITabBar____TabBar: UITabBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
