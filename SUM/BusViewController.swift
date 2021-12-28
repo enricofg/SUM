307a7e25c9edf1f59974a0b072a7ef7299dc30a5
jmachado100 <jose-machado96@hotmail.com>
2021-12-28 11:28:09 +0000

    Created new view controller with scroll

--- SUM/BusViewController.swift
+++ SUM/BusViewController.swift
@@ -9,29 +9,41 @@ import UIKit
 
 class BusViewController: UIViewController {
 
+
     @IBOutlet weak var OrigemTF: UITextField!
     @IBOutlet weak var AutocarroTF: UITextField!
-    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var lotacaoLB: UILabel!
+    @IBOutlet weak var DataLB: UILabel!
     
     let networkManager = NetworkManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        spinner.isHidden = false
-        spinner.startAnimating()
         
         networkManager.fetchBus { [weak self] (bus) in
             DispatchQueue.main.async {
-                self?.spinner.isHidden = true
-                self?.spinner.stopAnimating()
-                
+              
                 self?.AutocarroTF.text = bus.first?.Bus_Name
             }
         }
         
+        DataLB.text = getDate()
+        
     }
     
-
+    func getDate() -> String {
+           let date = Date()
+           let calendar = Calendar.current
+           
+           let day = calendar.component(.day, from: date)
+           let month = calendar.component(.month, from: date)
+           let year = calendar.component(.year, from: date)
+           let hour = calendar.component(.hour, from: date)
+           let minute = calendar.component(.minute, from: date)
+           
+           return "\(day)/\(month)/\(year) \(hour):\(minute)"
+       }
+    
 
 }
