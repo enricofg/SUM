0e501c31c1410c15d3e0db39307eb680387d1f8b
jmachado100 <jose-machado96@hotmail.com>
2021-12-28 11:50:47 +0000

    Added ma-view to bus view controller

--- SUM/BusViewController.swift
+++ SUM/BusViewController.swift
@@ -6,20 +6,29 @@
 //
 
 import UIKit
+import MapKit
 
-class BusViewController: UIViewController {
+class BusViewController: UIViewController, MKMapViewDelegate {
 
 
     @IBOutlet weak var OrigemTF: UITextField!
     @IBOutlet weak var AutocarroTF: UITextField!
     @IBOutlet weak var lotacaoLB: UILabel!
     @IBOutlet weak var DataLB: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
     
     let networkManager = NetworkManager()
+    let customPin = MKPointAnnotation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        mapView.delegate = self
+
+        let coordinate = CLLocationCoordinate2D(latitude: 39.73594501415817, longitude: -8.817790583959448)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
         
         networkManager.fetchBus { [weak self] (bus) in
             DispatchQueue.main.async {
