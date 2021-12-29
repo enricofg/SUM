cd8a1dadab8c87d70db43b3bcfe1cf5542371838
Luis <luiscmsousa@gmail.com>
2021-12-29 19:01:22 +0000

    add table view cell

---
+++ SUM/TableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  TableViewCell.swift
+//  ppppppp
+//
+//  Created by Luís Sousa on 27/12/2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    static let identifier = "TableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "TableViewCell", bundle: nil)
+    }
+    @IBOutlet var button: UIButton!
+    @IBAction func didTapButton(){
+        
+    }
+    
+    func configure(with title: String){
+        button.setTitle(title, for: .normal)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        button.setTitleColor(.link, for: .normal)
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
