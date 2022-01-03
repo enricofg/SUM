3778b200afd1c7dadb41c58feb479c17e68beaf7
Enrico Florentino <enricogomes@live.com>
2022-01-03 20:04:34 +0000

    Treat requests as failed by default

--- SUM/ARViewController.swift
+++ SUM/ARViewController.swift
@@ -28,51 +28,68 @@ class ARViewController: UIViewController {
         // Add the BusScene anchor to the scene
         arView.scene.anchors.append(busScene)
         
+        //Remove useless bars
+        let progressBar = busScene.progress
+        progressBar?.children.remove((progressBar?.children[0])!)
+        progressBar?.children.remove((progressBar?.children[1])!)
+        progressBar?.children.remove((progressBar?.children[1])!)
+        progressBar?.children.remove((progressBar?.children[2])!)
+        
+        ///Default Params:
+        //Control variables
+        var description = "Erro - Falha ao carregar"
+        var capacity = 0
+        
+        //3D Model variables - Bar
+        let barEntity : Entity = (progressBar?.children[1].children[0])!
+        var barModelComp: ModelComponent = (barEntity.components[ModelComponent.self])!
+        var barMaterial = SimpleMaterial()
+        barMaterial.color = .init(tint: (self.hexStringToUIColor(hex: "#222222"))).self
+        barModelComp.materials[0] = barMaterial
+        
+        //3D Model variables - Text
+        let valueEntity: Entity = (progressBar?.children[0].children[0].children[0])!
+        var valueModelComp: ModelComponent = (valueEntity.components[ModelComponent.self])!
+        var valueMaterial = SimpleMaterial()
+        valueMaterial.color = .init(tint: (self.hexStringToUIColor(hex: "#FFFFFF"))).self
+        valueModelComp.materials[0] = valueMaterial
+        
+        valueModelComp.mesh = .generateText("\(description)",
+                                                extrusionDepth: 0.001,
+                                        font: .systemFont(ofSize: 0.03),
+                              containerFrame: CGRect(),
+                                   alignment: .left,
+                               lineBreakMode: .byCharWrapping)
+        
+        progressBar?.children[0].children[0].children[0].components.set(valueModelComp)
+        progressBar?.children[1].children[0].components.set(barModelComp)
+        
         networkManager.fetchBus(busNumber: 5) { [weak self] (bus) in
             DispatchQueue.main.async {
                 
-                let capacity = bus.first?.Bus_Capacity ?? 0
-                var description = ""
-                                
-                let progressBar = busScene.progress
-                progressBar?.children.remove((progressBar?.children[0])!)
-                progressBar?.children.remove((progressBar?.children[1])!)
-                progressBar?.children.remove((progressBar?.children[1])!)
-                progressBar?.children.remove((progressBar?.children[2])!)
-                                
-                let barEntity : Entity = (progressBar?.children[1].children[0])!
-                var barModelComp: ModelComponent = (barEntity.components[ModelComponent.self])!
-                var barMaterial = SimpleMaterial()
-                //var scale: SIMD3<Float>(1.0, 1.0, 1.0)
+                capacity = bus.first?.Bus_Capacity ?? 0
                 if(capacity<40){
                     barMaterial.color = .init(tint: (self?.hexStringToUIColor(hex: "#3e9a2c"))!).self
-                    description="Vazio"
+                    description="Vazio - Lotação: \(capacity)%"
                     barEntity.scale = [1,1+Float(capacity/80),1]
                 } else if (capacity>=40&&capacity<=60) {
                     barMaterial.color = .init(tint: (self?.hexStringToUIColor(hex: "#e1cd00"))!).self
-                    description="Médio"
+                    description="Médio - Lotação: \(capacity)%"
                     barEntity.scale = [1,1.25+Float(capacity/90),1]
                 } else {
                     barMaterial.color = .init(tint: (self?.hexStringToUIColor(hex: "#bb1111"))!).self
-                    description="Cheio"
+                    description="Cheio - Lotação: \(capacity)%"
                     barEntity.scale = [1,1.5+Float(capacity/100),1]
                 }
-                    //barEntity.scale = [1,1+Float(capacity/75),1]
                     barModelComp.materials[0] = barMaterial
                 
-                let valueEntity: Entity = (progressBar?.children[0].children[0].children[0])!
-                var valueModelComp: ModelComponent = (valueEntity.components[ModelComponent.self])!
-                var valueMaterial = SimpleMaterial()
-                //valueMaterial.color = .init(tint: .white).self
-                valueMaterial.color = .init(tint: (self?.hexStringToUIColor(hex: "#FFFFFF"))!).self
-                    valueModelComp.materials[0] = valueMaterial
-                    valueModelComp.mesh = .generateText("\(description) - Lotação: \(capacity)%",
+                valueModelComp.mesh = .generateText("\(description)",
                                                         extrusionDepth: 0.001,
                                                 font: .systemFont(ofSize: 0.03),
                                       containerFrame: CGRect(),
                                            alignment: .left,
                                        lineBreakMode: .byCharWrapping)
-
+                
                 progressBar?.children[0].children[0].children[0].components.set(valueModelComp)
                 progressBar?.children[1].children[0].components.set(barModelComp)
             }
