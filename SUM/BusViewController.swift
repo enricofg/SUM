76851d0a4633aaf7dc3e37e3941b34e345bc5798
jmachado100 <jose-machado96@hotmail.com>
2022-01-04 19:40:30 +0000

    Added picker view to bus origin

--- SUM/BusViewController.swift
+++ SUM/BusViewController.swift
@@ -19,11 +19,26 @@ class BusViewController: UIViewController, MKMapViewDelegate {
     
     let networkManager = NetworkManager()
     let customPin = MKPointAnnotation()
+    private var _stops: [Stops]?
+    var locationManager: CLLocationManager?
+    var selectedRating : Int = 0
+    var pickerView = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         mapView.delegate = self
+        
+        networkManager.fetchStops { [weak self] (stops) in
+            self?._stops = stops
+            DispatchQueue.main.async {
+                self?.pickerView.reloadComponent(0)
+
+            }
+        }
+        
+        getUserLocation()
+    
 
         let coordinate = CLLocationCoordinate2D(latitude: 39.73594501415817, longitude: -8.817790583959448)
         let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
@@ -38,6 +53,13 @@ class BusViewController: UIViewController, MKMapViewDelegate {
         }
         
         DataLB.text = getDate()
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        OrigemTF.inputView = pickerView
+        
+        let locationImage=UIImage(systemName: "location.fill")
+        addLeftImageTo(txtField: OrigemTF, andImage: locationImage!)
         
     }
     
@@ -54,5 +76,64 @@ class BusViewController: UIViewController, MKMapViewDelegate {
            return "\(day)/\(month)/\(year) \(hour):\(minute)"
        }
     
+    func getUserLocation() {
+         locationManager = CLLocationManager()
+         locationManager?.requestAlwaysAuthorization()
+         locationManager?.startUpdatingLocation()
+     }
+   
+   func getDistance(myPositionLatitude: CLLocationDegrees , myPositionLongitude: CLLocationDegrees , pointPositionLatitude:CLLocationDegrees?, pointPositionLongitude:CLLocationDegrees?)->Double
+   {
+       let deviceLocation = CLLocation(latitude: myPositionLatitude , longitude: myPositionLongitude)
+       let pointLocation = CLLocation(latitude: pointPositionLatitude ?? myPositionLatitude, longitude: pointPositionLongitude ?? myPositionLongitude)
+
+       let distance = deviceLocation.distance(from: pointLocation) / 1000
+       return distance
+       
+   }
+    
+    func addLeftImageTo(txtField: UITextField, andImage img:UIImage){
+        let leftImageView = UIImageView(frame:CGRect(x:0.0,y:0.0,width:img.size.width,height:img.size.height))
+        leftImageView.image = img;
+        txtField.rightView = leftImageView;
+        txtField.rightViewMode = .always;
+    }
+    
+
+}
+
+
+extension BusViewController : UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if (_stops == nil)
+        {
+            return 1
+        }
+        return _stops!.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+       
+        if (_stops == nil)
+        {
+            return nil
+        }
+        let currentLoc = locationManager?.location
+
+        return "\(_stops![row].Stop_Name) \(getDistance(myPositionLatitude:(currentLoc?.coordinate.latitude)!, myPositionLongitude: (currentLoc?.coordinate.longitude)!, pointPositionLatitude: 59.326354, pointPositionLongitude: 18.072310))"
+    }
+   
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if (_stops != nil)
+        {
+            selectedRating = _stops![row].Stop_Id
+            OrigemTF.text = "\(_stops![row].Stop_Name)"
+
+            OrigemTF.resignFirstResponder()
+        }
+    }
 
 }
